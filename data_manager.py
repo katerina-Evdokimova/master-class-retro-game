83c67724e66f99dfdbfb97a1271c9abcfc85c2ec
KseniaNayshka <58656050+KseniaNayshka@users.noreply.github.com>
2024-12-22 13:57:52 +0300

    update

--- data_manager.py
+++ data_manager.py
@@ -1,11 +1,39 @@
 import os
 import sys
 import pygame
+from parameters import params
 
 
 class DataManager:
     def __init__(self):
-        pass
+        self.ship = {
+            'columns': 4,
+            'rows': 1,
+            'path': 'ship.png',
+            'frames': []
+        }
+
+        self.ship_enemy = {
+            'columns': 4,
+            'rows': 1,
+            'path': 'ship_enemy.png',
+            'frames': []
+        }
+
+        self.particle = {
+            'columns': 1,
+            'rows': 3,
+            'path': 'laser_bullets.png',
+            'frames': []
+        }
+
+        self.background = self.load_image('background.png')
+        self.ship['frames'] = self.cut_sheet(self.load_image(self.ship['path'], True),
+                                             self.ship['columns'], self.ship['rows'])
+        self.ship_enemy['frames'] = self.cut_sheet(self.load_image(self.ship_enemy['path'], True),
+                                                   self.ship_enemy['columns'], self.ship_enemy['rows'])
+        self.particle['frames'] = self.cut_sheet(self.load_image(self.particle['path'], True),
+                                                 self.particle['columns'], self.particle['rows'])
 
     def load_image(self, name, alpha=False):
         fullname = os.path.join('data', name)
@@ -15,12 +43,28 @@ class DataManager:
 
         image = pygame.image.load(fullname)
 
-        # if alpha:
-        #     image = image.convert_alpha()
-        # else:
-        #     image = image.convert()
+        if alpha:
+            image = image.convert_alpha()
+        else:
+            image = image.convert()
 
         return image
 
+    def cut_sheet(self, sheet, columns, rows):
+        '0, 0 - координаты левого верхнего угла'
+        'sheet.get_width() // columns, sheet.get_height() // rows - ширина и высота'
+        # создаем прямоугольник
+        self.rect = pygame.Rect(0, 0, sheet.get_width() // columns, sheet.get_height() // rows)
+        # цикл для создания готовых фрэймов наших спрайтов,
+        # те обрезаем каждую картинку и создаем из готовых изображений список для дальнейшего использования.
+        frames = []
+        for j in range(rows):
+            for i in range(columns):
+                frame_location = (self.rect.w * i, self.rect.h * j)
+                frames.append(pygame.transform.scale(sheet.subsurface(pygame.Rect(frame_location, self.rect.size)),
+                                                     (params.SIZE_SPRITES, params.SIZE_SPRITES)))
+        return frames
 
+pygame.init()
+screen = pygame.display.set_mode((1, 1))
 data_manager = DataManager()
--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import pygame
 import random
 from data_manager import data_manager
-from OtherSprites import Sprite, EnemyShips
-from  parameters import params
-
+from other_sprites import Sprite, ShipEnemy, Particle
+from parameters import params
 
 
 def draw_score(screen):
@@ -16,10 +15,6 @@ def draw_score(screen):
 
 
 class Ship(Sprite):
-    columns = 4
-    rows = 1
-    image = data_manager.load_image('ship.png')
-
     def __init__(self, x: int, y: int):
         """Конструктор
 
@@ -28,119 +23,168 @@ class Ship(Sprite):
             y int: коррдината размещения корабля
         """
         super().__init__(all_sprites, ship_group)
-        self.frames = [] # список со всеми, нарезанными спрайтами
-        self.cut_sheet(Ship.image, Ship.columns, Ship.rows)
-
-        self.cur_frame = 0 # номер первого спрайта
-        self.image = self.frames[self.cur_frame] # присваивание картинки
-        self.rect = self.rect.move(x, y) # размещение на экране
+        # список со всеми, нарезанными спрайтами
+        self.frames = data_manager.ship['frames']
+        self.columns = data_manager.ship['columns']
+        self.rows = data_manager.ship['rows']
+        self.cur_frame = 0  # номер первого спрайта
+        self.image = self.frames[self.cur_frame]  # присваивание картинки
+        self.rect = self.image.get_rect()
+        self.rect = self.rect.move(x, y)  # размещение на экране
 
         # координаты спрайта
         self.x = x
         self.y = y
 
         # скорость корабля
-        self.v = 100
+        self.v = 1
 
-        self.time = pygame.time.get_ticks()
+        self.time = 0
 
         # ФЛАГИ
-        self.flag = {pygame.K_LEFT: False, pygame.K_RIGHT: False, pygame.K_UP: False, pygame.K_DOWN: False}
- 
+        self.flag_up = False
+        self.flag_down = False
+        self.flag_left = False
+        self.flag_right = False
+
     def is_live(self):
         """Проверка на кол-во жизней
 
         Returns:
             bool: True - корабль потерпел крушение, False - еще жив
         """
-        return self.cur_frame == Ship.columns - 1
- 
-    def update(self, *args):
+        return self.cur_frame == self.columns - 1
+
+    def update(self, *args, v=0):
+        self.v += v
         FPS = params.FPS
+        self.time += 1
 
         # Проверка на столкновение нашего корабля и корабля противника
         sprite = pygame.sprite.spritecollideany(self, enemy_group)
         if sprite:
             # если произошло столкновение, то отнимаем жизнь и 5 очков
             self.cur_frame += 1
-            if self.cur_frame < Ship.columns - 1:
-                self.image = self.frames[self.cur_frame] # меняем картинку спрайта на более побитую
+            if self.cur_frame < self.columns - 1:
+                self.image = self.frames[self.cur_frame]  # меняем картинку спрайта на более побитую
                 params.set_score(-5)
             else:
-                self.kill() # если закончились жизни, корабль погибает
-                params.set_score(-params.get_score()) # счет равен 0
+                self.kill()  # если закончились жизни, корабль погибает
+                params.set_score(-params.get_score())  # счет равен 0
             sprite.kill()
-        
-        
-        # ЗДЕСЬ ПИШЕМ ЛОГИКУ ДВИЖЕНИЯ КОРАБЛЯ
-        
-        
-        # ЗДЕСЬ ЛОГИКУ ПОЯВЛЕНИЯ ПУЛЬ
 
-            
-        
-            
+        # ЗДЕСЬ ПИШЕМ ЛОГИКУ ДВИЖЕНИЯ КОРАБЛЯ
+        if args and args[0].type == pygame.KEYDOWN:
+            if args[0].key == pygame.K_RIGHT:
+                self.flag_right = True
+            elif args[0].key == pygame.K_LEFT:
+                self.flag_left = True
+            elif args[0].key == pygame.K_UP:
+                self.flag_up = True
+            elif args[0].key == pygame.K_DOWN:
+                self.flag_down = True
+
+        if args and args[0].type == pygame.KEYUP:
+            if args[0].key == pygame.K_RIGHT:
+                self.flag_right = False
+            elif args[0].key == pygame.K_LEFT:
+                self.flag_left = False
+            elif args[0].key == pygame.K_UP:
+                self.flag_up = False
+            elif args[0].key == pygame.K_DOWN:
+                self.flag_down = False
+
+        if self.flag_left:
+            self.x -= self.v
+        if self.flag_down:
+            self.y += self.v
+        if self.flag_right:
+            self.x += self.v
+        if self.flag_up:
+            self.y -= self.v
+
+        if self.y < 0:
+            self.y = 0
+        elif self.y > height - self.rect.height:
+            self.y = height - self.rect.height
+        elif self.x < 0:
+            self.x = 0
+        elif self.x > width - self.rect.width:
+            self.x = width - self.rect.width
+
+        self.rect = self.image.get_rect().move(
+            self.x, self.y
+        )
+
+        # ЗДЕСЬ ЛОГИКУ ПОЯВЛЕНИЯ ПУЛЬ
+        if self.time >= 30:
+            Particle(self.x, self.y - params.SIZE_SPRITES // 2, all_sprites, particle_group)
+            self.time = 0
 
 
 def main():
     running = True
-    times = pygame.time.get_ticks()
-    while running: 
+    clock = pygame.time.Clock()
+    time = 0
+    while running:
+        time += 1
         if ship.is_live():
             return True
         # внутри игрового цикла ещё один цикл
         # приема и обработки сообщений
-        screen.blit(bg, (0, 0)) # отображение фона на экране
-        
+        screen.blit(bg, (0, 0))  # отображение фона на экране
+
         for event in pygame.event.get():
-				# при закрытии окна
-				
-                if event.type == pygame.QUIT:
-                    return False
-                ship_group.update(event)
-		
-		# отрисовка и изменение свойств объектов
+            # при закрытии окна
+
+            if event.type == pygame.QUIT:
+                return False
+            ship_group.update(event)
+
+        # отрисовка и изменение свойств объектов
         enemy_group.draw(screen)
         particle_group.draw(screen)
         all_sprites.draw(screen)
         ship_group.draw(screen)
-        enemy_group.update(particle_group)
-        ship_group.update()
-        particle_group.update()
-        draw_score(screen)
-		# обновление экрана
-        pygame.display.flip()
-        time.tick(params.FPS)
 
-        if len(enemy_group.sprites()) <= N // 2 or abs(times - pygame.time.get_ticks()) >= 3000:
+        # каждые 20 очков идет увеличение скорости
+        v = 0
+        if not (params.get_score() + 1) % 20:
+            v = 0.1
+
+        enemy_group.update(particle_group, v)
+        ship_group.update(v=v)
+        particle_group.update(v=v)
+        draw_score(screen)
+
+        # обновление экрана
+        pygame.display.flip()
+        clock.tick(params.FPS)
+
+        if len(enemy_group.sprites()) <= N // 2 or time >= 180:
             for x, y in [(random.randrange(1, width), -25) for _ in range(N)]:
-                EnemyShips(x, y, all_sprites, enemy_group)
-            times = pygame.time.get_ticks()
-    
+                ShipEnemy(x, y, all_sprites, enemy_group)
+            time = 0
 
 
 if __name__ == '__main__':
-    running = True
-    
     # цикл для вечной игры
-    while running:
-        pygame.init()
+    pygame.init()
 
-        params.set_score(-params.get_score())
-        N = 5
-        time = pygame.time.Clock()
-        all_sprites = pygame.sprite.Group()
-        enemy_group = pygame.sprite.Group()
-        particle_group = pygame.sprite.Group()
-        ship_group = pygame.sprite.Group()
+    params.set_score(-params.get_score())
+    N = 5
+    time = pygame.time.Clock()
+    all_sprites = pygame.sprite.Group()
+    enemy_group = pygame.sprite.Group()
+    particle_group = pygame.sprite.Group()
+    ship_group = pygame.sprite.Group()
 
-        size = width, height = 500, 600
-        screen = pygame.display.set_mode(size)
-        bg = pygame.transform.scale(data_manager.load_image('background.png'), size)
+    size = width, height = 500, 600
+    screen = pygame.display.set_mode(size)
+    bg = pygame.transform.scale(data_manager.background, size)
 
-        for x, y in [(random.randrange(1, width), -25) for _ in range(N)]:
-            EnemyShips(x, y, all_sprites, enemy_group)
+    for x, y in [(random.randrange(1, width), -25) for _ in range(N)]:
+        ShipEnemy(x, y, all_sprites, enemy_group)
 
-        ship = Ship(width // 2, height - params.SIZE_SPRITES)
-        running = main()
-    
+    ship = Ship(width // 2, height - params.SIZE_SPRITES)
+    main()
